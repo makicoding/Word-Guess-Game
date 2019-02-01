4d1abdc7ada2ee097e76e19680a595e5689489d9
Maki Hirose <Capture@Makis-MBP-RETINA-13.local>
2019-02-01 13:07:41 -0500

    190201 1:07pm Commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,9 @@
-// Initial guesses remaining
-var guessesRemaining = 15;
+// Initial variables
+var correctGuessScore = 0;
+var guessesRemaining = 5;
+
+// Play Again Button hidden initially
+document.getElementById("playAgainButton").style.display = 'none';
 
 // This function begins the game when 
 // the user presses a key on their keyboard
@@ -12,6 +16,12 @@ document.onkeyup = function(userKeyStroke){
         var letterM = 'M';
         document.getElementById("letter1").innerHTML = letterM;
         console.log('user typed m');
+        correctGuessScore++;
+        if (correctGuessScore === 5){
+            var winText = 'You Win!'
+            document.getElementById("youWinText").innerHTML = winText
+            document.getElementById("playAgainButton").style.display = 'block';
+        }
     }
 
     else if (userChoice === "a"){
@@ -19,18 +29,36 @@ document.onkeyup = function(userKeyStroke){
         document.getElementById("letter2").innerHTML = letterA;
         document.getElementById("letter7").innerHTML = letterA;
         console.log('user typed a');
+        correctGuessScore++;
+        if (correctGuessScore === 5){
+            var winText = 'You Win!'
+            document.getElementById("youWinText").innerHTML = winText
+            document.getElementById("playAgainButton").style.display = 'block';
+        }
     }
 
     else if (userChoice === "d"){
         var letterD = 'D';
         document.getElementById("letter3").innerHTML = letterD;
         console.log('user typed d');
+        correctGuessScore++;
+        if (correctGuessScore === 5){
+            var winText = 'You Win!'
+            document.getElementById("youWinText").innerHTML = winText
+            document.getElementById("playAgainButton").style.display = 'block';
+        }
     }
 
     else if (userChoice === "o"){
         var letterO = 'O';
         document.getElementById("letter4").innerHTML = letterO;
         console.log('user typed o');
+        correctGuessScore++;
+        if (correctGuessScore === 5){
+            var winText = 'You Win!'
+            document.getElementById("youWinText").innerHTML = winText
+            document.getElementById("playAgainButton").style.display = 'block';
+        }
     }
 
     else if (userChoice === "n"){
@@ -38,11 +66,30 @@ document.onkeyup = function(userKeyStroke){
         document.getElementById("letter5").innerHTML = letterN;
         document.getElementById("letter6").innerHTML = letterN;
         console.log('user typed n');
+        correctGuessScore++;
+        if (correctGuessScore === 5){
+            var winText = 'You Win!'
+            document.getElementById("youWinText").innerHTML = winText
+            document.getElementById("playAgainButton").style.display = 'block';
+        }
     }
 
     else {
         guessesRemaining--;
         document.getElementById("userGuessesRemaining").innerHTML = guessesRemaining;
         console.log('user score goes down by 1 point');
+        if (guessesRemaining === 0){
+            alert('Game Over!');
+            window.location.reload();
+        }
     }
 }
+
+onclick
+
+
+// This code will reload a page:
+// window.location.reload();
+
+// This code will load a different page:
+// window.location.href = "http://google.com";
